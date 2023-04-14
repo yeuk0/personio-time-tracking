91fcd85b40b687049884507ed06c6ff3e59a37d9
Diego Barrantes <diegob@empathy.co>
2023-04-14 22:23:37 +0200

    Moving last methods to Navigator class

--- navigator.py
+++ navigator.py
@@ -1,3 +1,5 @@
+from date_utils import get_today_date, get_days_range_from_date_month, is_valid_day
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,7 +23,8 @@ class Navigator:
 
     def zoom_out(self):
         self.browser.set_context("chrome")
-        self.browser.find_element(By.TAG_NAME, "html").send_keys(Keys.COMMAND + '-')
+        self.browser.find_element(
+            By.TAG_NAME, "html").send_keys(Keys.COMMAND + '-')
         self.browser.set_context("content")
 
     def go_to_attendance_page(self):
@@ -31,5 +34,31 @@ class Navigator:
             By.TAG_NAME, 'form').find_element(By.TAG_NAME, 'a')
         attendance_link.click()
 
+    def get_day_buttons(self):
+        attendance_table = self.browser.find_element(By.ID, 'attendance')
+        day_buttons = {}
+        date = get_today_date()
+        for day_number in get_days_range_from_date_month(date):
+            date_str = f"{date.strftime('%Y-%m')}-{day_number+1:02d}"
+            if is_valid_day(date_str):
+                day = self.__get_day_button(attendance_table, date_str)
+                day_buttons[date_str] = day.find_element(By.TAG_NAME, 'button')
+        return day_buttons
+
+    def open_input_dialog(self, day_button):
+        day_button.click()
+        return self.find_element_by_and_wait(self.browser, By.XPATH, "//section[@role='dialog']")
+
+    def close_input_dialog(self, dialog):
+        close_button = self.find_element_by_and_wait(dialog, By.XPATH, "//button[@data-test-id='day-entry-dialog-close-button']")
+        close_button.click()
+
     def quit(self):
         self.browser.quit()
+
+    def __get_day_button(element, date_str):
+        try:
+            return Navigator.find_element_by_and_wait(element, By.XPATH, f"//div[@data-test-id='day_{date_str}']")
+        except TimeoutException:
+            # This should only happen once as we're handling valid days values
+            return Navigator.find_element_by_and_wait(element, By.XPATH, "//div[@data-test-id='today-cell']")
