aa3e7b2021a552cb58aba9014f460c0f82070b34
Diego Barrantes <diegob@empathy.co>
2023-04-14 21:48:45 +0200

    Minor fix

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 
 
